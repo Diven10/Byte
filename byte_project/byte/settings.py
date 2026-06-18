93a2ce79e4829718f46f32d36cc635078aed50d0
Diven10 <cosmicbubble63@gmail.com>
2026-06-19 02:31:15 +0530

    Add dj-database-url for Postgres cloud database support

--- byte_project/byte/settings.py
+++ byte_project/byte/settings.py
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'byte.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# If DATABASE_URL is set in the environment (e.g., on Vercel), use it
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=False)
+if db_from_env:
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
